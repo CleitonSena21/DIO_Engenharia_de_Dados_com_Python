d2e21f486cbd4b79c20cdbf203633ea3ca84ce18
Cleiton Sena <cleitonsena21@hotmail.com>
2024-09-01 12:24:55 -0300

    Create string_1.py

---
+++ Manipulando_Strings/conhecendo_metodos_uteis_da_classe_string/string_1.py
@@ -0,0 +1,20 @@
+nome = "cLeItOn"
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+texto = "  Hello World!  "
+
+print(texto + ".")
+print(texto.strip() + "." )
+print(texto.rstrip() + ".")
+print(texto.lstrip() + ".")
+
+menu = "Python"
+
+print("####" + menu + "####")
+print(menu.center(14))
+print(menu.center(14, "#"))
+print("P_y_t_h_o_n")
+print("_".join(menu))
