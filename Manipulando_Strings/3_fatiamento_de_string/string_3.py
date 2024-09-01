c291d591305d398da7e694c74827010c7efaef02
Cleiton Sena <cleitonsena21@hotmail.com>
2024-09-01 14:37:45 -0300

    commit

---
+++ Manipulando_Strings/3_fatiamento_de_string/string_3.py
@@ -0,0 +1,9 @@
+nome = "Cleiton Gomes De Sena"
+
+print(nome[0])
+print(nome[:7])
+print(nome[8:])
+print(nome[17:21])
+print(nome[17:21:2])
+print(nome[:])
+print(nome[::-1])
---
+++ Manipulando_Strings/string_4.py
@@ -0,0 +1,23 @@
+nome = 'Cleiton'
+
+mensagem = f"""
+    Olá meu nome é {nome},
+Eu estou aprendendo python.
+      Essa mensagem tem diferentes recuos.nome
+"""
+
+print(mensagem)
+
+print(
+    """
+    =============== MENU ===============
+
+    1 - Depositar
+    2 - Sacar
+    0 - Sair
+
+    ====================================
+     
+                  Obrigado!
+"""
+)
